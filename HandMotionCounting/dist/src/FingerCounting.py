3efc7fe97706ddc3c4dffbdd7a37ac63f3a8ba2b
Mei Romney <mei.romney987@gmail.com>
2025-11-29 20:32:41 +0100

    Implement finger counting feature

--- HandMotionCounting/dist/src/FingerCounting.py
+++ HandMotionCounting/dist/src/FingerCounting.py
@@ -23,16 +23,39 @@ pTime = 0
 
 detector = htm.handDetector(detectionCon = 0.75)
 
+tipIds = [4, 8, 12, 16, 20]
+
 while True: 
     success, img = cap.read()
-    img = detector.findHands()
+    img = detector.findHands(img)
     lmList = detector.findPosition(img, draw = False)
-    print(lmList)
+    # print(lmList)
 
     if len(lmList) != 0:
+        fingers = []
 
-    h, w, c = overlayList[0].shape
-    img[0:h, 0:w] = overlayList[0]
+        # Thumb
+        if lmList[tipIds[0]][1] > lmList[tipIds[0] - 1][1]:
+            fingers.append(1)
+        else:
+            fingers.append(0)
+
+        # 4 fingers
+        for id in range(1, 5):
+            if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                fingers.append(1)
+            else:
+                fingers.append(0)
+
+        #print(fingers)
+        totalFingers = fingers.count(1)
+        print(totalFingers)
+
+        h, w, c = overlayList[totalFingers - 1].shape
+        img[0:h, 0:w] = overlayList[totalFingers - 1]
+
+        cv2.rectangle(img, (20, 225), (170, 425), (0, 255, 0), cv2.FILLED)
+        cv2.putText(img, str(totalFingers), (45, 375), cv2.FONT_HERSHEY_PLAIN, 10, (255, 0, 0), 25)
 
     cTime = time.time()
     fps = 1/(cTime-pTime)
